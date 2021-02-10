1e0ac9524a05a82efca7a90c8e7e077d669d1296
mvga <ing.mvasquezg@gmail.com>
2021-02-09 21:09:21 -0600

    Creando el programa 04 del capitulo 02 del libro

---
+++ comoProgramarEnJavaSeptimaEdicion/chapter02/Programa04.java
@@ -0,0 +1,13 @@
+/**
+ *Libro: Como Programar en Java Septima Edicion Deitel
+ *Capitulo 02: Introduccion a las aplicaciones en Java
+ *Programa 04: Programa que imprime un mensaje en pantalla en varias lineas usando el el printf que indica el formato.
+*/
+
+public class Programa04{
+    
+    public static void main(String[] args){
+        System.out.printf("%s\n%s\n", "Bienvenido", "la programacion en Java!");
+    }//end main
+
+}//end class Programa04
