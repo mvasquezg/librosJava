e18b0634f85e16b8d4a05ecd4d7a77ac5b0da3ad
mvga <ing.mvasquezg@gmail.com>
2021-02-09 20:34:08 -0600

    Creando el programa 01 del capitulo 2 del libro

---
+++ comoProgramarEnJavaSeptimaEdicion/chapter02/Programa01.java
@@ -0,0 +1,13 @@
+/**
+ *Libro: Como Programar en Java Septima Edicion Deitel
+ *Capitulo 02: Introduccion a las aplicaciones en Java
+ *Programa 01: Programa que imprime un mensaje en pantalla en una liena de texto
+*/
+
+public class Programa01{
+    
+    public static void main(String[] args){
+        System.out.println("Hola mundo. Bienvenido a la programacion en Java");
+    }//end main
+
+}//end class Programa01
