9eef530d67d2b3c071bcefdc51bd0d0ea8b49274
mvga <ing.mvasquezg@gmail.com>
2021-02-09 20:49:36 -0600

    Creando el programa 02 del capitulo 2 del libro

---
+++ comoProgramarEnJavaSeptimaEdicion/chapter02/Programa02.java
@@ -0,0 +1,14 @@
+/**
+ *Libro: Como Programar en Java Septima Edicion Deitel
+ *Capitulo 02: Introduccion a las aplicaciones en Java
+ *Programa 02: Programa que imprime un mensaje en pantalla en una liena de texto con dos instrucciones, print y println.
+*/
+
+public class Programa02{
+    
+    public static void main(String[] args){
+        System.out.print("Bienvenido a "); 
+        System.out.println("la programacion en Java");
+    }//end main
+
+}//end class Programa02
