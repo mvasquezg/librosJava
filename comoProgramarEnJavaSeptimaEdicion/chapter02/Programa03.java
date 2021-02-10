a2ec4191991bc3f118659e23b0cf2a636f1a6697
mvga <ing.mvasquezg@gmail.com>
2021-02-09 21:00:26 -0600

    Creando el programa 03 del capitulo 02 del libro

---
+++ comoProgramarEnJavaSeptimaEdicion/chapter02/Programa03.java
@@ -0,0 +1,13 @@
+/**
+ *Libro: Como Programar en Java Septima Edicion Deitel
+ *Capitulo 02: Introduccion a las aplicaciones en Java
+ *Programa 03: Programa que imprime un mensaje en pantalla en varias lineas de texto con una sola instrucion println.
+*/
+
+public class Programa03{
+    
+    public static void main(String[] args){
+        System.out.print("Bienvenido\na\nla programacion\nen Java");
+    }//end main
+
+}//end class Programa03
