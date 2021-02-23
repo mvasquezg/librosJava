d34569707bed435cf4bb3970fdcc532b34b5c0b1
mvga <ing.mvasquezg@gmail.com>
2021-02-23 12:01:44 -0600

    Creando programa 14 mprimiendo figuras, cuadrado, ovalo, cruz y rombo con puro print sin usar ciclos del capitulo 02 del libro

---
+++ comoProgramarEnJavaSeptimaEdicion/chapter02/Programa14.java
@@ -0,0 +1,30 @@
+/**
+ *Libro: Como Programar en Java Septima Edicion Deitel
+ *Capitulo 02: Introduccion a las aplicaciones en Java
+ *Programa 14: Escriba un programa que dibuje las siguientes figuras, 
+ *    *********         ***         *            *          
+ *    *       *      *      *      ***          * *
+ *    *       *     *        *    *****        *    *   
+ *    *       *     *        *      *         *      *
+ *    *       *     *        *      *        *        *
+ *    *       *     *        *      *         *      *
+ *    *       *     *        *      *          *    *
+ *    *       *      *      *       *           * *
+ *    *********         ***         *            *
+*/
+public class Programa14{
+    
+    public static void main(String[] args){
+        
+        System.out.print("*********        ***         *            *\n"+          
+     "*       *      *     *      ***          * *\n"+
+     "*       *     *       *    *****        *    *\n"+   
+     "*       *     *       *      *         *      *\n"+
+     "*       *     *       *      *        *        *\n"+
+     "*       *     *       *      *         *      *\n"+
+     "*       *     *       *      *          *    *\n"+
+     "*       *      *     *       *           * *\n"+
+     "*********        ***         *            *\n\n");
+    }//end main
+
+}//end class Programa14
