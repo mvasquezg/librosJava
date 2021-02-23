16ee0e8977aa32c137020f17c8c9145a4b551617
mvga <ing.mvasquezg@gmail.com>
2021-02-23 12:06:06 -0600

    Creando programa 15 imprimiendo asteriscos del capitulo 02 del libro

---
+++ comoProgramarEnJavaSeptimaEdicion/chapter02/Programa15.java
@@ -0,0 +1,45 @@
+/**
+ *Libro: Como Programar en Java Septima Edicion Deitel
+ *Capitulo 02: Introduccion a las aplicaciones en Java
+ *Programa 15: Que imprimen los siguientes lineas de codigo 
+ *a)    System.out.println("*\n**\n***\n****\n*****");          
+ *b)    System.out.println("*");
+ *      System.out.println("***");
+ *      System.out.println("*****");   
+ *      System.out.println("****");
+ *      System.out.println("**);
+ *c)    System.out.print("*");
+ *      System.out.print("***");
+ *      System.out.print("*****");   
+ *      System.out.print("****");
+ *      System.out.println("**);   
+ *d)    System.out.print("*");
+ *      System.out.println("***");
+ *      System.out.println("*****");   
+ *      System.out.print("****");
+ *      System.out.println("**);      
+ */    
+public class Programa15{
+    
+    public static void main(String[] args){
+      System.out.println("*\n**\n***\n****\n*****\n\n");          
+      
+      System.out.println("*");
+      System.out.println("***");
+      System.out.println("*****");   
+      System.out.println("****");
+      System.out.println("**\n");
+      
+      System.out.print("*");
+      System.out.print("***");
+      System.out.print("*****");   
+      System.out.print("****");
+      System.out.println("**\n");   
+      
+      System.out.print("*");
+      System.out.println("***");
+      System.out.println("*****");   
+      System.out.print("****");
+      System.out.println("**\n");
+    }//end main
+}//end class Programa15
