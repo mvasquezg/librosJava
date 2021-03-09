110e3de840172b65616c24464e3fbe60c1804f77
mvga <ing.mvasquezg@gmail.com>
2021-03-09 09:15:47 -0600

    Creando el programa 01 del capitulo 01, que imprime el mensaje Hola mundo en pantalla.

---
+++ Java02CursoDeProgramacion/chapter01/HolaMundo.java
@@ -0,0 +1,12 @@
+/*
+ *Libro: Java 2 Curso de Programacion 4ta Edicion
+ *Capitulo 01: Fases en el Desarrollo de un Programa
+ *Programa 01: Programa que imprime un mensaje en pantalla "Hola Mundo"
+*/
+
+
+class HolaMundo{
+    public static void main(String[] args){
+        System.out.println("Hola Mundo");
+    }//end main
+}//end HolaMundo
