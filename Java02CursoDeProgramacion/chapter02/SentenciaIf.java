53fd7dfdce908ff025043484f31201c1250c5437
mvga <ing.mvasquezg@gmail.com>
2021-03-09 15:47:03 -0600

    Creando el programa 03 del capitulo 02, que ejecuta un condicional con la sentencia if

---
+++ Java02CursoDeProgramacion/chapter02/SentenciaIf.java
@@ -0,0 +1,17 @@
+/*
+ *Libro: Java 2 Curso de Programacion 4ta Edicion
+ *Capitulo 02: Fases en el Desarrollo de un Programa
+ *Programa 03: Aplicando la sentencia if para determinar si un numero es par. 
+*/
+
+class SentenciaIf{
+    public static void main(String[] args){
+        int num=24;
+
+        if(num%2==0){
+            System.out.println("Numero Par");
+        }
+
+        System.out.println("Valor: "+num);
+    }//end main
+}//end SentenciaIf
