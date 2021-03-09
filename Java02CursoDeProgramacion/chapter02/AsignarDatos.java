12cc1f47c899fec1b1e55511e75723b2a58065c0
mvga <ing.mvasquezg@gmail.com>
2021-03-09 14:33:41 -0600

    Creando el programa 01 del capitulo 02, se creando tres variables dos int y un double y una referencia String

---
+++ Java02CursoDeProgramacion/chapter02/AsignarDatos.java
@@ -0,0 +1,24 @@
+/*
+ *Libro: Java 2 Curso de Programacion 4ta Edicion
+ *Capitulo 02: Fases en el Desarrollo de un Programa
+ *Programa 01: Declaracion de tres variables numericas a, b, c y una referencias s a una cadena de caracteres
+ *despues asigna valores a esa variables.
+*/
+
+class AsignarDatos{
+    public static void main(String[] args){
+        //declaracion de variables
+        double a=0, b=0;
+        int c=0;
+
+        //declaracion de una referencia
+        String s;
+
+        //asignacion de varible
+        a=3.14; b=2.71; c=2;
+
+        //asignacion de refrencia
+        s="Datos";
+
+    }//end main
+}//end AsignarDatos
