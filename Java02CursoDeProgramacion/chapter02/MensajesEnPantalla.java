c7afee6fc99d38694eacc92e232cdcc4e059ba72
mvga <ing.mvasquezg@gmail.com>
2021-03-09 17:06:23 -0600

    Creando programa 05 del capitulo 02, que imprime mensaje en pantalla en dos lineas.

---
+++ Java02CursoDeProgramacion/chapter02/MensajesEnPantalla.java
@@ -0,0 +1,14 @@
+/*
+ *Libro: Java 2 Curso de Programacion 4ta Edicion
+ *Capitulo 02: Fases en el Desarrollo de un Programa
+ *Programa 06: Imprimir en pantalla los siguientes Mensajes:
+ * Bienvenido al Mundo Java.
+ * Podras dar solucion a muchos problemas. 
+*/
+
+class MensajesEnPantalla{
+    
+    public static void main(String[] args){
+        System.out.println("Bienvenido al mundo Java.\nPodras dar solucion a muchas problemas.");
+    }//end main
+}//end MensajesEnPantalla
